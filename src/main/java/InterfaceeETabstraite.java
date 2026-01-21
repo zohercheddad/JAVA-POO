3234e680cda79db040eca47d3ac453f865a408c9
zohercheddad <57918430+zohercheddad@users.noreply.github.com>
2026-01-22 00:39:28 +0100

    Merge remote-tracking branch 'origin/main'

--- src/main/java/InterfaceeETabstraite.java
+++ src/main/java/InterfaceeETabstraite.java
@@ -1,80 +1,65 @@
 public class InterfaceeETabstraite {
 
-// ================================
-// Classe principale (point d’entrée)
-// ================================
-public class Main {
-    public static void main(String[] args) {
+    // ================================
+    // Point d’entrée (classe interne statique)
+    // ================================
+    public static class Main {
+        public static void main(String[] args) {
 
-        // Polymorphisme : même type Animal, objets différents
-        Animal dog = new Dog();
-        Animal cat = new Cat();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
 
-        dog.swim();
-        dog.play();
+            dog.swim();
+            dog.play();
 
-        cat.swim();
-        cat.play();
-    }
-}
-
-
-// ================================
-// Interface : capacité à nager
-// ================================
-interface Swimmer {
-    void swim();
-}
-
-
-// ================================
-// Interface : capacité à jouer
-// ================================
-interface Player {
-    void play();
-}
-
-
-// ================================
-// Classe abstraite Animal
-// - Implémente les deux interfaces
-// - Sert de base commune aux animaux
-// ================================
-abstract class Animal implements Swimmer, Player {
-    // Pas d’implémentation ici (sera faite dans les classes filles)
-}
-
-
-// ================================
-// Classe Dog
-// ================================
-class Dog extends Animal {
-
-    @Override
-    public void swim() {
-        System.out.println("Dog is swimming");
+            cat.swim();
+            cat.play();
+        }
     }
 
-    @Override
-    public void play() {
-        System.out.println("Dog is playing");
-    }
-}
-
-
-// ================================
-// Classe Cat
-// ================================
-class Cat extends Animal {
-
-    @Override
-    public void swim() {
-        System.out.println("Cat is swimming");
+    // ================================
+    // Interfaces
+    // ================================
+    interface Swimmer {
+        void swim();
     }
 
-    @Override
-    public void play() {
-        System.out.println("Cat is playing");
+    interface Player {
+        void play();
     }
-}
+
+    // ================================
+    // Classe abstraite
+    // ================================
+    abstract static class Animal implements Swimmer, Player {
+    }
+
+    // ================================
+    // Implémentations
+    // ================================
+    static class Dog extends Animal {
+
+        @Override
+        public void swim() {
+            System.out.println("Dog is swimming");
+        }
+
+        @Override
+        public void play() {
+            System.out.println("Dog is playing");
+        }
+    }
+
+    static class Cat extends Animal {
+
+        @Override
+        public void swim() {
+            System.out.println("Cat is swimming");
+        }
+
+        @Override
+        public void play() {
+            System.out.println("Cat is playing");
+        }
+    }
 }
